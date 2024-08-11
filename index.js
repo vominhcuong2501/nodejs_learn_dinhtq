a142223c42d2e2dfeca9b0c4ef56255d7e0d00cb
vominhcuong2501 <minhcuongvo2501@gmail.com>
2024-08-11 12:06:41 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
