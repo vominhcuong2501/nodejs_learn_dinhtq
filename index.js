a30e89126c77ebcc057141e39f38b3ef622a5f94
vominhcuong2501 <minhcuongvo2501@gmail.com>
2024-08-11 11:51:53 +0700

    add verrcel json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -9,3 +12,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
